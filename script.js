1379f086c249187d61dcd6b583b7855236b1be3a
Geethaka Sankalpa <geethaka.20231659@iit.ac.lk>
2024-08-02 11:43:52 +0530

    updated adding and removing tasks functionalities

---
+++ script.js
@@ -0,0 +1,26 @@
+// variables to get inputs and button
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === '' ){
+        alert("You Must write something!")
+    } else {
+        let li = document.createElement("li");  // create HTML element
+        li.innerHTML = inputBox.value;  // add to the input field
+        listContainer.appendChild(li);      // add the input at the end      
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    } 
+    inputBox.value = '';    // clear input box
+}
+
+listContainer.addEventListener("click", function(e){
+    //  name of the element where the event occurred:
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+    } else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
